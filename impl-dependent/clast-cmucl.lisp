f7f2e8d3c5f2790ba418fc548957b9cdf3906b42
Marco Antoniotti <marco.antoniotti@unimib.it>
2014-02-21 19:11:19 +0100

    File added.

---
+++ impl-dependent/clast-cmucl.lisp
@@ -0,0 +1,57 @@
+;;;; -*- Mode: Lisp -*-
+
+;;;; clast-cmucl.lisp --
+;;;; Code lifted from IT.BESE.ARNESI and CL-WALKER.
+;;;;
+;;;; Environment functions.
+;;;; Either you have CLtL2 environment functions or your
+;;;; implementation sucks.
+
+(in-package "CLAST")
+
+;;;; The Magnificent (yet neglected) 7.
+;;;; CLtL2 environment manipulation manipulation functions.
+
+(defun variable-information (variable &optional env)
+  (ext:variable-information variable env)
+  )
+
+
+(defun function-information (f &optional env)
+  (ext:function-information f env)
+  )
+
+
+(defun declaration-information (decl-name &optional env)
+  (ext:function-information decl-name env)
+  )
+
+
+(defun augment-environment (env &rest keys ; Just a utility variable.
+                                &key
+                                variable
+                                symbol-macro
+                                function
+                                macro
+                                declare)
+  (declare (ignore variable symbol-macro function macro declare))
+  (apply #'ext:augment-environment env keys)
+  )
+
+
+(defmacro define-declaration (decl-name lambda-list &body forms)
+  `(ext:define-declaration ,decl-name ,lambda-list ,@forms)
+  )
+
+
+(defun parse-macro (name lambda-list body &optional env)
+  (ext:parse-macro name lambda-list body env)
+  )
+
+
+(defun enclose (lambda-expression &optional env)
+  (ext:enclose lambda-expression env)
+  )
+
+
+;;;; end of file -- clast-cmucl.lisp --
