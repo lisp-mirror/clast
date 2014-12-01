13e5516c60b03599e99cfdbb2c19f65173070950
Marco Antoniotti <marco.antoniotti@unimib.it>
2014-12-01 15:06:50 +0100

    Fixed ACL environment access.

--- impl-dependent/clast-allegro.lisp
+++ impl-dependent/clast-allegro.lisp
@@ -16,7 +16,7 @@
 ;;;; CLtL2 environment manipulation manipulation functions.
 ;;;;
 ;;;; Franz redefined the order of the return values.  Hence this
-;;;; implementation wrapper is more complicated.
+;;;; implementation wrapper is needed.
 
 (defmacro reorder-*information-values-portably (env-function-form)
   `(multiple-value-bind (binding-type
@@ -33,22 +33,63 @@
      ))
 
 
-(defun variable-information (variable &optional env)
+(defun ensure-parsing-environment (&optional env)
+  (sys:ensure-portable-walking-environment env))
+
+
+(defmethod is-environment ((e sys::augmentable-environment)) t)
+
+
+(defun get-implementation-env (env) ; This is needed for the internal API.
+  env)
+
+
+;;;; Morevoer, ACL needs a workaround to handle a "portable walking
+;;;; environment.
+
+(defvar *allegro-portable-parsing-env*
+  ;; (sys:ensure-portable-walking-environment nil)
+  (ensure-parsing-environment))
+
+
+(defun variable-information (variable
+                             &optional
+                             (env *allegro-portable-parsing-env*))
   (reorder-*information-values-portably
-   (sys:variable-information f env t))
+   (sys:variable-information variable env t))
   )
 
 
-(defun function-information (f &optional env)
+(defun function-information (f
+                             &optional
+                             (env *allegro-portable-parsing-env*))
   (reorder-*information-values-portably
    (sys:function-information f env t t))
   )
 
 
-(defun declaration-information (decl-name &optional env)
+(defun declaration-information (decl-name
+                                &optional
+                                (env *allegro-portable-parsing-env*))
   (reorder-*information-values-portably
    (sys:declaration-information decl-name env))
   )
+
+
+(defun tag-information (tag-name
+                        &optional
+                        (env *allegro-portable-parsing-env*))
+  (multiple-value-bind (tag-kwd locative decl)
+      (sys:tag-information tag-name env)
+    (values tag-kwd decl locative)))
+
+
+(defun block-information (block-name
+                          &optional
+                          (env *allegro-portable-parsing-env*))
+  (multiple-value-bind (block-kwd locative decl)
+      (sys:block-information block-name env)
+    (values block-kwd decl locative)))
 
 
 (defun augment-environment (env &rest all-keys ; Just a utility variable.
@@ -60,7 +101,9 @@
                                 declare
                                 &allow-other-keys)
   (declare (ignore variable symbol-macro function macro declare))
-  (apply #'sys:augment-environment env all-keys)
+  (apply #'sys:augment-environment
+         (or env *allegro-portable-parsing-env*)
+         all-keys)
   )
 
 
@@ -69,13 +112,19 @@
   )
 
 
-(defun parse-macro (name lambda-list body &optional env)
-  (error "No implementation for CLtL2 PARSE-MACRO in Allegro CL.")
+(defun parse-macro (name lambda-list body
+                         &optional
+                         (env *allegro-portable-parsing-env*))
+  ;; (error "No implementation for CLtL2 PARSE-MACRO in Allegro CL.")
+  (excl::defmacro-expander (list* name lambda-list body) env)
   )
 
 
-(defun enclose (lambda-expression &optional env)
-  (error "No implementation for CLtL2 ENCLOSE in Allegro CL.")
+(defun enclose (lambda-expression
+                &optional
+                (env *allegro-portable-parsing-env*))
+  ;; (error "No implementation for CLtL2 ENCLOSE in Allegro CL.")
+  (excl::make-lexical-closure lambda-expression nil env)
   )
 
 
