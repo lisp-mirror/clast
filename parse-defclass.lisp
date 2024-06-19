a0637b6d9de46e0eb74e02a138e0a64639efcf3b
Marco Antoniotti <marco.antoniotti@unimib.it>
2024-06-19 08:22:26 +0200

    Factored out classes definition.

--- parse-defclass.lisp
+++ parse-defclass.lisp
@@ -17,66 +17,7 @@
 ;;;; Parsing DEFCLASS is not as complicated as parsing DEFSTRUCT but
 ;;;; it still requires some hairy code.
 ;;;;
-;;;; As per LOOP two kinds of sub-forms are defined to deal with slots
-;;;; and options.
-
-
-;;;----------------------------------------------------------------------------
-;;; defclass-subform
-;;; class-option-subform, class-slot-subform, class-slot-option-subform
-
-(defclass defclass-subform (form) ())
-
-
-(defclass class-slot-subform (defclass-subform)
-  ((name :reader class-slot-subform-name
-         :initarg :slot-name) ; Inherited; new reader and initarg added.
-   (options :reader class-slot-subform-options
-                  :initarg :slot-options)
-   )
-  )
-
-
-(defun is-class-slot-subform (x)
-  (typep x 'class-slot-subform))
-
-
-(defun class-slot-subform-p (x)
-  (is-class-slot-subform x))
-
-
-(defmethod clast-element-subforms ((sos class-slot-subform))
-  (list (class-slot-subform-name sos)
-        (class-slot-subform-options sos)
-        ))
-
-
-(defclass class-option-subform (defclass-subform)
-  ((name :reader class-option-subform-name
-         :initarg :option-name) ; Inherited; new reader and initarg added.
-   (spec :reader class-option-subform-spec
-         :initarg :option-spec)
-   )
-  )
-
-
-(defun is-class-option-subform (x)
-  (typep x 'class-option-subform))
-
-
-(defun class-option-subform-p (x)
-  (is-class-option-subform x))
-
-
-(defun make-class-option-form (option-name &rest spec)
-  (make-instance 'class-option-subform
-                 :option-name option-name
-                 :option-spec spec))
-
-
-(defmethod clast-element-subforms ((sos class-option-subform))
-  (list (class-option-subform-name sos)
-        (class-option-subform-spec sos)))
+;;;; See also 'class-defclass-elements' file.
 
 
 ;;;---------------------------------------------------------------------------
